9e0df4f146c1201725ef35906fc7b4a5ff9d5377
Aghiles-Belkalem <belkalemaghiles@gmail.com>
2024-12-03 15:27:57 +0100

    creatin an API for good actions & creating the fetch for quotes and actions

---
+++ client/src/services/Actions.ts
@@ -0,0 +1,16 @@
+export const quotesZenFetch = () => {
+	return fetch("http://localhost:3310/good_actions")
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error("Erreur lors du chargement");
+			}
+			return response.json();
+		})
+		.then((data) => {
+			return data;
+		})
+		.catch((error) => {
+			console.error("Erreur", error);
+			throw error;
+		});
+};
