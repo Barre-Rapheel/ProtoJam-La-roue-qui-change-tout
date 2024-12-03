7d3b0b335be01c3c43033d101cf8d6bad3ade39e
Raphaël <barre.raphael97@gmail.com>
2024-12-03 18:05:38 +0100

    Update US_Header-Footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,19 +7,18 @@ function App() {
   return (
     <>
       <Header />
-      <main className="main">
-        <h2>Bienvenue sur La Roue Qui Change Tout !</h2>
+      <main>
+        <div className="card-container">
+          <Card
+            title="Bonne action du jour :"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium tempora architecto rerum eveniet nisi!"
+          />
+          <Card
+            title="Citation du jour :"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium tempora architecto rerum eveniet nisi!"
+          />
+        </div>
       </main>
-      <div className="card-container">
-        <Card
-          title="Bonne action du jour :"
-          content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium tempora architecto rerum eveniet nisi!"
-        />
-        <Card
-          title="Citation du jour :"
-          content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium tempora architecto rerum eveniet nisi!"
-        />
-      </div>
       <Footer />
     </>
   );
