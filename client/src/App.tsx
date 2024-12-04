53ded867e79f85093109ec5f381371816a00ef12
Aghiles-Belkalem <belkalemaghiles@gmail.com>
2024-12-04 11:54:29 +0100

    running both serv & all functions ok

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,44 +8,44 @@ import { goodActionsFetch } from "./services/Actions";
 import { quotesZenFetch } from "./services/QuotesFetch";
 
 function App() {
-	const [action, setAction] = useState<string>("");
-	const [quote, setQuote] = useState<string>("");
+  const [action, setAction] = useState<string>("");
+  const [quote, setQuote] = useState<string>("");
 
-	const handleSpinComplete = async (randomId: number) => {
-		try {
-			const data = await goodActionsFetch(randomId);
-			setAction(data?.action || "Aucune citation disponible");
-			fetchQuote();
-		} catch (error) {
-			console.error("Erreur lors de la récupération de l'action", error);
-			setAction("Erreur lors de la récupération de l'action");
-		}
-	};
+  const handleSpinComplete = async (randomId: number) => {
+    try {
+      const data = await goodActionsFetch(randomId);
+      setAction(data?.action || "Aucune citation disponible");
+      fetchQuote();
+    } catch (error) {
+      console.error("Erreur lors de la récupération de l'action", error);
+      setAction("Erreur lors de la récupération de l'action");
+    }
+  };
 
-	const fetchQuote = async () => {
-		try {
-			const data = await quotesZenFetch();
-			console.info(data);
-			if (data && data.length > 0) {
-				setQuote(data[0]);
-			}
-		} catch (error) {
-			console.error("Erreur lors de la récupération de la citation", error);
-			setQuote("Erreur lors de la récupération de la citation");
-		}
-	};
+  const fetchQuote = async () => {
+    try {
+      const data = await quotesZenFetch();
+      console.info(data);
+      if (data && data.length > 0) {
+        setQuote(data[0]);
+      }
+    } catch (error) {
+      console.error("Erreur lors de la récupération de la citation", error);
+      setQuote("Erreur lors de la récupération de la citation");
+    }
+  };
 
-	return (
-		<>
-			<Header />
-			<MySpinWheel onSpinComplete={handleSpinComplete} />
-			<div className="card-container">
-				<Card title="Action du jour :" content={action} />
-				<Card title="Citation du jour :" content={quote} />
-			</div>
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <MySpinWheel onSpinComplete={handleSpinComplete} />
+      <div className="card-container">
+        <Card title="Action du jour :" content={action} />
+        <Card title="Citation du jour :" content={quote} />
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
--- client/src/services/Actions.ts
+++ client/src/services/Actions.ts
@@ -1,18 +1,18 @@
 export const goodActionsFetch = (id: number) => {
-	const url = `http://localhost:3310/good_actions/${id}`;
+  const url = `http://localhost:3310/good_actions/${id}`;
 
-	return fetch(url)
-		.then((response) => {
-			if (!response.ok) {
-				throw new Error("Error loading");
-			}
-			return response.json();
-		})
-		.then((data) => {
-			return data;
-		})
-		.catch((error) => {
-			console.error("Erreur", error);
-			throw error;
-		});
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Error loading");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.error("Erreur", error);
+      throw error;
+    });
 };
