95a14f5765d7943d5789a72a92247e7821aa3546
Aghiles-Belkalem <belkalemaghiles@gmail.com>
2024-12-04 14:24:48 +0100

    last version of the project

--- client/src/components/Card/index.tsx
+++ client/src/components/Card/index.tsx
@@ -1,27 +1,27 @@
 import "./Card.css";
 
 interface CardProps {
-  title: string;
-  content:
-    | string
-    | {
-        q: string;
-        a: string;
-      };
+	title: string;
+	content:
+		| string
+		| {
+				q: string;
+				a: string;
+		  };
 }
 
 export default function Card({ title, content }: CardProps) {
-  return (
-    <article className="card">
-      <h3 className="card-title">{title}</h3>
-      {typeof content === "object" ? (
-        <blockquote>
-          {content.q}
-          <cite>{content.a}</cite>
-        </blockquote>
-      ) : (
-        <p className="card-content">{content}</p>
-      )}
-    </article>
-  );
+	return (
+		<article className="card">
+			<h3 className="card-title">{title}</h3>
+			{typeof content === "object" ? (
+				<blockquote className="card-content">
+					{content.q}
+					<cite className="card-content"> {content.a}</cite>
+				</blockquote>
+			) : (
+				<p className="card-content">{content}</p>
+			)}
+		</article>
+	);
 }
